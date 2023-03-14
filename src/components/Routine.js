823f9df35f6d2062374c631833dceaa9ceb53d77
Donovan <donovangriffin04@gmail.com>
2023-03-13 18:02:01 -0700

    final adjustments of routine page before styling

--- src/components/Routine.js
+++ src/components/Routine.js
@@ -1,22 +1,18 @@
 export default function Routine ({ selectedStretches, count }){
       console.log(selectedStretches)  
 
-  
   return (    
     <div>
-      {          
+        {
         selectedStretches.map((stretches) => {
           return(
-            <div key={stretches.name}>
-              {stretches.name}
-              
+            <div>
+            <h1 key={stretches.name}>{stretches.name}</h1>
               {stretches.directions.map((direction)=> {
                 return <p>{direction}</p>
-              })}
+            })}
             </div>
-          )
-        })
-      }
+        )})}
     </div>
   )
 }
