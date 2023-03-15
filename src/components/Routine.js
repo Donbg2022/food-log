30bf20f30dd203568aaa476c3dfe88243adea9b4
Donovan <donovangriffin04@gmail.com>
2023-03-15 11:57:12 -0700

    painstakenly added a onclick feature that changes bg color. will be updated

--- src/components/Routine.js
+++ src/components/Routine.js
@@ -2,16 +2,17 @@ import { useState } from "react"
 import Countdown from "./Countdown"
 
 export default function Routine ({ selectedStretches }){
-
-  const handleClick = () => {
+  const [isSelected, setIsSelected] = useState([])
+  const handleClick = (index) => {
+    setIsSelected([...isSelected, index])
     }
 
   return (    
     <div >
         {
-        selectedStretches.map((stretches) => {
+        selectedStretches.map((stretches,index) => {
           return(
-            <div className="routine-card"key={stretches.name}>
+            <div onClick={() => handleClick(index)} style={{backgroundColor: isSelected.includes(index) ? 'green': 'blue'}} className="routine-card"key={stretches.name}>
               
             <h1 className="stretch-title" key={stretches.name}>{stretches.name}</h1>
               {stretches.directions.map((direction)=> {
